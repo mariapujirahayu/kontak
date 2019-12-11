f51f8276be2872114c46bdd517a30cdb7cefb2d1
mariapujirahayu <mariapujir@gmail.com>
2019-12-11 21:46:48 +0700

    Initial commit

---
+++ app/src/main/java/id/twoh/kontak/MainActivity.java
@@ -0,0 +1,68 @@
+package id.twoh.kontak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+import id.twoh.kontak.Adapter.AdapterKontak;
+import id.twoh.kontak.Model.GetKontak;
+import id.twoh.kontak.Model.Kontak;
+import id.twoh.kontak.Rest.ApiClient;
+import id.twoh.kontak.Rest.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    Button btIns;
+    ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static MainActivity ma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btIns = (Button) findViewById(R.id.btIns);
+        btIns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InsertActivity.class));
+            }
+        });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        ma=this; refresh();
+    }
+
+    public void refresh() {
+        Call<GetKontak> kontakCall = mApiInterface.getKontak();
+        kontakCall.enqueue(new Callback<GetKontak>() {
+            @Override
+            public void onResponse(Call<GetKontak> call, Response<GetKontak> response) {
+                List<Kontak> KontakList = response.body().getListDataKontak();
+                Log.d("Retrofit Get", "Jumlah data Kontak: " + String.valueOf(KontakList.size()));
+                mAdapter = new AdapterKontak(KontakList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+            @Override
+            public void onFailure(Call<GetKontak> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+}
